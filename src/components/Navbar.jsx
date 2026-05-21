7f6ce41a6aa05abdf9b8c7bb78e97e12be8a5f2f
khuma66-git <khuma66@gmail.com>
2026-05-21 14:50:36 -0700

    NavLink Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import "../index.css"
 
 const Navbar = () => {
   return (
     <div>
       <ul>
-        <Link to="/"><li>Home</li></Link>
-        
-        <Link to="login"><li>Login</li></Link>
+        <NavLink className={(e)=>{return e.isActive?"pur":""}} to="/"><li>Home</li></NavLink>
+        <NavLink className={(e)=>{return e.isActive?"pur":""}} to="/login"><li>Login</li></NavLink>
+        {/* <NavLink className={(e)=>{return e.isActive?"pur":""}} to="user"><li>User</li></NavLink>        navigate to user become /login/user */}
+        <NavLink className={(e)=>{return e.isActive?"pur":""}} to="/user"><li>User</li></NavLink>        
+        <NavLink className={(e)=>{return e.isActive?"pur":""}} to="username"><li>Username</li></NavLink>        
       </ul>
     </div>
   )
